35144a043bd38fce07fc28d09f9600e00339cd6e
Juan Vanegas <eatteer@gmail.com>
2022-10-31 08:34:06 -0500

    Fix wrapping items of listing tags component

--- resources/views/components/listing-tags.blade.php
+++ resources/views/components/listing-tags.blade.php
@@ -2,9 +2,9 @@
 @php
     $tags = explode(',', $csvTags);
 @endphp
-<ul class="flex">
+<ul class="flex flex-wrap gap-2">
     @foreach($tags as $tag)
-        <li class="bg-black text-white rounded-xl px-3 py-1 mr-2">
+        <li class="bg-black text-white rounded-xl px-3 py-1">
             <a href="/?tag={{$tag}}">{{$tag}}</a>
         </li>
     @endforeach
